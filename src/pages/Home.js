276e5cb7b7cdb18081802eb0701408880ec87d47
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-08-18 19:59:49 +1200

    starting header

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <header>
+        <div>Hope Storage</div>
+        <div>
+          <div></div>
+          <div>027 454 3813</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
