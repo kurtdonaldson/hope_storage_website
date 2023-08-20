b7d1ce5dcb952b8ddb5b19ab0b699e2cbb9ae60b
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-08-20 19:06:01 +1200

    contact button added

---
+++ src/components/SafeStorage.js
@@ -0,0 +1,14 @@
+function SafeStorage() {
+  return (
+    <div className="safeStorage">
+      <div className="safeStorage_heading_div">
+        <h2>Safe and secure storage</h2>
+      </div>
+      <div className="contactBtnDiv">
+        <button>Contact Us</button>
+      </div>
+    </div>
+  );
+}
+
+export default SafeStorage;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
+import SafeStorage from "../components/SafeStorage";
 import logo from "../images/hope_storage_logo.png";
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
     <div className="home_div">
       <Header />
       <Navigation />
+      <SafeStorage />
       <div className="image_div">
         <img className="logo" src={logo} alt="" />
       </div>
