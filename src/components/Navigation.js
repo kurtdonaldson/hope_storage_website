60cba993189c865a3edece0d834c65f3bf1fbf69
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-08-20 15:30:28 +1200

    navbar added

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function Navigation() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container className="d-flex justify-content-center">
+        <Nav className="navlinks d-flex justify-content-between">
+          <Nav.Link href="#home">What we offer</Nav.Link>
+          <Nav.Link href="#features">How it works</Nav.Link>
+          <Nav.Link href="#pricing">How to find us</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,12 @@
-import "../styles/home.css";
-import React from "react";
-import { FaPhoneAlt } from "react-icons/fa";
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 
 function Home() {
   return (
-    <header className="d-flex justify-content-between align-items-center container-fluid">
-      <div className="h1_div d-flex align-items-center">
-        <h1 id="h1">Hope Storage</h1>
-      </div>
-      <div className="phone_number_div d-flex align-items-center justify-content-between">
-        <FaPhoneAlt className="phone_icon" />
-        <p className="header_phone_number m-0">027 454 3813</p>
-      </div>
-    </header>
+    <div>
+      <Header />
+      <Navigation />
+    </div>
   );
 }
 
