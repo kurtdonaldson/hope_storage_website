41d4cd95fe648ec2fb8e69dc6f58f072d06d8b71
Kurt Donaldson <kurt_donaldson@hotmail.com>
2023-11-11 14:52:34 +1300

    google maps integrated

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,6 +1,7 @@
 import "../styles/home.css";
 
 import SendMessageForm from "./SendMessageForm";
+import { Map } from "./Map";
 
 const ContactInfo = () => {
   return (
@@ -8,7 +9,9 @@ const ContactInfo = () => {
       <div>
         <SendMessageForm />
       </div>
-      <div className="mapDiv">Map</div>
+      <div className="mapDiv">
+        <Map />
+      </div>
       <div className="findUsDiv">
         <h2>How to find us</h2>
         <p>027 454 3813</p>
