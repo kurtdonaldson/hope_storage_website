38faa77d627ea40eb72a6bf4b7d76d0c0c14ead7
Kurt Donaldson <kurt_donaldson@hotmail.com>
2024-01-12 22:22:20 +1300

    contact footer map responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>© 2023 nicoladonaldson.com and kurtdonaldson.com</p>
+      <p>© 2023 nicoladonaldson.com</p>
     </div>
   );
 };
