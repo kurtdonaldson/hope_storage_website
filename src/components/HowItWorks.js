1fbd445befe847cba756b47506e25b7b9fef5ce3
Kurt Donaldson <kurt_donaldson@hotmail.com>
2024-01-12 21:30:35 +1300

    Responsiveness caravan banner

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -9,19 +9,31 @@ function HowItWorks() {
       <h2>How it works</h2>
       <div className="howItWorksImgContainer">
         <div className="howItWorksImg1 howItWorksImgDiv">
-          <img className="howItWorksImages" src={entrySignImg} alt="" />
-          <h3>Contact us</h3>
-          <p>Enquire online or give us a call.</p>
+          <div className="howItWorksImageInnerDiv">
+            <img className="howItWorksImages" src={entrySignImg} alt="" />
+          </div>
+          <div>
+            <h3>Contact us</h3>
+            <p>Enquire online or give us a call.</p>
+          </div>
         </div>
         <div className="howItWorksImg2 howItWorksImgDiv">
-          <img className="howItWorksImages" src={caravanImg} alt="" />
-          <h3>Arrange a time</h3>
-          <p>Look around - decide if we are right for you!</p>
+          <div className="howItWorksImageInnerDiv">
+            <img className="howItWorksImages" src={caravanImg} alt="" />
+          </div>
+          <div>
+            <h3>Arrange a time</h3>
+            <p>Look around - decide if we are right for you!</p>
+          </div>
         </div>
         <div className="howItWorksImg3 howItWorksImgDiv">
-          <img className="howItWorksImages" src={lockImg} alt="" />
-          <h3>Move in</h3>
-          <p>You’ll have 24/7 access.</p>
+          <div className="howItWorksImageInnerDiv">
+            <img className="howItWorksImages" src={lockImg} alt="" />
+          </div>
+          <div>
+            <h3>Move in</h3>
+            <p>You’ll have 24/7 access.</p>
+          </div>
         </div>
       </div>
     </div>
