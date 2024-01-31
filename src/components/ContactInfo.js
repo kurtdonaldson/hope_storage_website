ccd9e07ef682f4d62060b91374a507fc93c8d102
DONNY345 <89885417+DONNY345@users.noreply.github.com>
2024-02-01 09:37:15 +1300

    map hyperlink to Google maps added

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -10,11 +10,13 @@ const ContactInfo = () => {
         <SendMessageForm />
       </div>
       <div className="mapDiv">
-        <img
-          className="mapImg"
-          src={hopeStorageMap}
-          alt="Google Maps screenshot displaying the strategic location of our container storage facility, offering convenient access and security for business needs."
-        />
+        <a href="https://www.google.com/maps/place/Hope+Storage+Limited/@-41.3466485,173.145153,15z/data=!4m6!3m5!1s0x6d3b96928ff9a4c9:0xbf44f12e9009818!8m2!3d-41.3466485!4d173.145153!16s%2Fg%2F11h4dhytqj?entry=ttu">
+          <img
+            className="mapImg"
+            src={hopeStorageMap}
+            alt="Google Maps screenshot displaying the strategic location of our container storage facility, offering convenient access and security for business needs."
+          />
+        </a>
       </div>
       <div className="findUsDiv">
         <h2>How to find us</h2>
